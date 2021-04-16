cf5ab7767bb45ee22840382c5b945a53b7b93041
Neha <neha.oscprofessionals@gmail.com>
2021-04-16 14:01:24 +0530

    Add New File for choosing application

--- ProtocolExample/ColorsViewController.swift
+++ ProtocolExample/ColorsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ColorsViewController.swift
 //  ProtocolExample
 //
 //  Created by Neha Gupta on 16/04/21.
@@ -7,18 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ColorsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
     
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-
+    */
 
 }
-
