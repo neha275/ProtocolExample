30559913f734840e260021a7ad6fe324543a3a05
Neha <neha.oscprofessionals@gmail.com>
2021-04-16 13:44:10 +0530

    New protocol file is added

---
+++ ProtocolExample/BackgroundProtols.swift
@@ -0,0 +1,12 @@
+//
+//  BackgroundProtols.swift
+//  ProtocolExample
+//
+//  Created by Neha Gupta on 16/04/21.
+//
+
+import UIKit
+
+protocol BackgroundColor {
+    func setBackgroundColor(selectedColorIndex : Int)
+}
